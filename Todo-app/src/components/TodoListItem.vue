d5062e717896ea7ec317e03fed27f382936917b1
Elbourabi Said <elbourabisaid@MacBook-Pro-de-Elbourabi.local>
2025-01-31 10:55:08 +0100

    edit btns code

---
+++ Todo-app/src/components/TodoListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-text-field v-model="textFieldValue" label="Todo Item" dense hide-details></v-text-field>
+
+  <v-btn class="custom-color-1" @click="textFieldValueSave" icon="mdi-check" size="small" />
+  <v-btn class="custom-color-3" @click="textFieldValueEdit" icon="mdi-pencil-outline" size="small" />
+  <v-btn class="custom-color-5" @click="textFieldValueDelete" icon="mdi-trash-can-outline" size="small" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const textFieldValue = ref<string>('')
+
+const textFieldValueSave = () => {
+  console.log('---->', textFieldValue.value)
+}
+const textFieldValueEdit = () => {
+  console.log('---->', textFieldValue.value)
+}
+const textFieldValueDelete = () => {
+  console.log('---->', textFieldValue.value)
+}
+</script>
+<style lang="scss" scoped>
+.color-1 {
+  color: white;
+}
+</style>
